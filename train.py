cc12045023bcf51edec520ceb33711194bb02690
Matthew Burke <mattburke122@gmail.com>
2020-04-08 17:27:41 -0500

    removed duplicate criterion

--- train.py
+++ train.py
@@ -31,7 +31,7 @@ def single_gpu_train():
     criterion_d = torch.nn.BCELoss()
     # criterion_g = torch.nn.L1Loss()
     criterion_g_data = torch.nn.CrossEntropyLoss()
-    criterion_g_adv = torch.nn.BCELoss()  # need separate criterion to carry loss only through Gs?
+    # criterion_g_adv = torch.nn.BCELoss()  # need separate criterion to carry loss only through Gs? No
     optimizer_d = torch.optim.Adam([
         {'params': D1.parameters()},
         {'params': D2.parameters()}], lr=0.001)  # does sharing parameters make sense?
@@ -62,10 +62,9 @@ def single_gpu_train():
             FAKE_t = torch.full((BATCH_SIZE,), FAKE, device=device)  # tensor of FAKE labels
 
             D1_loss = criterion_d(prob_g1_pred, FAKE_t) + criterion_d(prob_g1_gt, REAL_t)  # call loss.backward on both individually? or together is ok?
-            G1_adv_loss = criterion_g_adv(prob_g1_pred, REAL_t)
+            G1_adv_loss = criterion_d(prob_g1_pred, REAL_t)
             # L_cgan(G1, D1) = D1_loss + G1_adv_loss
             L_cgan1 = D1_loss + G1_adv_loss
-
 
             # g2_input = torch.cat((original_image, shadow_mask), 1)
             g2_input = g1_gt_cat
@@ -83,17 +82,18 @@ def single_gpu_train():
             g2_gt_cat = torch.cat((original_image, g1_output, seg_edges_gt), 1)
             g2_pred_cat = torch.cat((original_image, g1_output, g2_output), 1)
 
-            prob_g2_gt = D2(g2_gt_cat).detach()
+            # prob_g2_gt = D2(g2_gt_cat).detach()
+            prob_g2_gt = D2(g2_gt_cat)
             prob_g2_pred = D2(g2_pred_cat)
             # D2_loss = criterion_d(prob_g2_pred, prob_g2_gt)
             D2_loss = criterion_d(prob_g2_pred, FAKE_t) + criterion_d(prob_g2_gt, REAL_t)
-            G2_adv_loss = criterion_g_adv(prob_g2_pred, REAL_t)
+            G2_adv_loss = criterion_d(prob_g2_pred, REAL_t)
             L_cgan2 = D2_loss + G2_adv_loss
 
             # loss = G1_loss + lambda1 * G2_loss + lambda2 * D1_loss + lambda3 * D2_loss
             loss = L_data1 + lambda1 * L_data2 + lambda2 * L_cgan1 + lambda3 * L_cgan2
             print('Epoch: %d | iter: %d | train loss: %.10f' % (epoch, i, float(loss)))
-            if epoch % 2000 < 1000:
+            if epoch % 6 < 3:
                 optimizer_d.zero_grad()
                 loss.backward()  # only uses D1_loss and D2_loss?
                 optimizer_d.step()
