c6bfb812d9549c6520c1bf12edbd2aa6f96501c5
Matthew Burke <mattburke122@gmail.com>
2020-04-08 16:53:42 -0500

    changed loss calculations based on paper

--- train.py
+++ train.py
@@ -4,13 +4,16 @@ from Stcgan_net import *
 import torch
 import torch.utils.data as Data
 import os
+import random
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+REAL = 1
+FAKE = 0
 
 #hyperparam
 BATCH_SIZE = 5
-lambda1 = 5
+lambda1 = 5  #high due to L1Loss instead of BCELoss?
 lambda2 = 0.1
 lambda3 = 0.1
 
@@ -25,13 +28,14 @@ def single_gpu_train():
     D2 = Discriminator_second().to(device)
 
 
-    criterion_d = torch.nn.BCELoss(size_average=False)
+    criterion_d = torch.nn.BCELoss()
     # criterion_g = torch.nn.L1Loss()
-    criterion_g = torch.nn.CrossEntropyLoss()
-    optimizerd = torch.optim.Adam([
+    criterion_g_data = torch.nn.CrossEntropyLoss()
+    criterion_g_adv = torch.nn.BCELoss()  # need separate criterion to carry loss only through Gs?
+    optimizer_d = torch.optim.Adam([
         {'params': D1.parameters()},
         {'params': D2.parameters()}], lr=0.001)  # does sharing parameters make sense?
-    optimizerg = torch.optim.Adam([
+    optimizer_g = torch.optim.Adam([
         {'params': G1.parameters()},
         {'params': G2.parameters()}], lr=0.001)  # does sharing parameters make sense?
 
@@ -43,20 +47,32 @@ def single_gpu_train():
 
 
             g1_output = G1(original_image)
-            G1_loss = criterion_g(g1_output, seg_gt)
+            G1_loss = criterion_g_data(g1_output, seg_gt)  # L_data1(G1)
+            L_data1 = G1_loss
 
             g1_pred_cat = torch.cat((original_image, g1_output), 1)
             g1_gt_cat = torch.cat((original_image, seg_gt), 1)
 
-            prob_g1_gt = D1(g1_gt_cat).detach()  # what does detatch do? Prevents backpropogation occuring through new variable
+            # prob_g1_gt = D1(g1_gt_cat).detach()  # what does detatch do? Prevents backpropogation occuring through new variable
+            prob_g1_gt = D1(g1_gt_cat)
             prob_g1_pred = D1(g1_pred_cat)
-            D1_loss = criterion_d(prob_g1_pred, prob_g1_gt)  # correct? loss b/w predictions != loss b/w pred and correct label
+            # D1_loss = criterion_d(prob_g1_pred, prob_g1_gt)  # correct? loss b/w predictions != loss b/w pred and correct label
+
+            REAL_t = torch.full((BATCH_SIZE,), REAL, device=device)  # tensor of REAL labels
+            FAKE_t = torch.full((BATCH_SIZE,), FAKE, device=device)  # tensor of FAKE labels
+
+            D1_loss = criterion_d(prob_g1_pred, FAKE_t) + criterion_d(prob_g1_gt, REAL_t)  # call loss.backward on both individually? or together is ok?
+            G1_adv_loss = criterion_g_adv(prob_g1_pred, REAL_t)
+            # L_cgan(G1, D1) = D1_loss + G1_adv_loss
+            L_cgan1 = D1_loss + G1_adv_loss
+
 
             # g2_input = torch.cat((original_image, shadow_mask), 1)
             g2_input = g1_gt_cat
             g2_output = G2(g2_input)
             seg_edges_gt = get_edges(g1_output, seg_gt)
-            G2_loss = criterion_g(g2_output, seg_edges_gt)
+            G2_loss = criterion_g_data(g2_output, seg_edges_gt)  #L_data2(G2|G1)
+            L_data2 = G2_loss
 
             # edges_if_seg_perfect = get_edges(seg_gt, seg_gt)  # torch.zeroes faster, but don't know h, w, BATCHSIZE
             # we want the d2 loss to capture the higher-order properties of the edges (that they are connected units)
@@ -69,20 +85,22 @@ def single_gpu_train():
 
             prob_g2_gt = D2(g2_gt_cat).detach()
             prob_g2_pred = D2(g2_pred_cat)
+            # D2_loss = criterion_d(prob_g2_pred, prob_g2_gt)
+            D2_loss = criterion_d(prob_g2_pred, FAKE_t) + criterion_d(prob_g2_gt, REAL_t)
+            G2_adv_loss = criterion_g_adv(prob_g2_pred, REAL_t)
+            L_cgan2 = D2_loss + G2_adv_loss
 
-            D2_loss = criterion_d(prob_g2_pred, prob_g2_gt)
-
-
-            loss = G1_loss + lambda1 * G2_loss + lambda2 * D1_loss + lambda3 * D2_loss
+            # loss = G1_loss + lambda1 * G2_loss + lambda2 * D1_loss + lambda3 * D2_loss
+            loss = L_data1 + lambda1 * L_data2 + lambda2 * L_cgan1 + lambda3 * L_cgan2
             print('Epoch: %d | iter: %d | train loss: %.10f' % (epoch, i, float(loss)))
             if epoch % 2000 < 1000:
-                optimizerd.zero_grad()
-                loss.backward()
-                optimizerd.step()
+                optimizer_d.zero_grad()
+                loss.backward()  # only uses D1_loss and D2_loss?
+                optimizer_d.step()
             else:
-                optimizerg.zero_grad()
+                optimizer_g.zero_grad()
                 loss.backward()
-                optimizerg.step()
+                optimizer_g.step()
 
         if epoch % 100 == 99:
             generator1_model = os.path.join("model/generator1_%d.pkl" % epoch)
