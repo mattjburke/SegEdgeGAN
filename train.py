f83dd9d0d86f1a473177c5ba5657ca2f6c8384be
Matthew Burke <mattburke122@gmail.com>
2020-04-11 17:59:46 -0500

    typo fix

--- train.py
+++ train.py
@@ -256,7 +256,9 @@ def single_gpu_train():
             # save losses and iou every epoch for graphing
             if mode == 'train':
                 # save every 25 iterations points
-                df = pd.DataFrame(list(zip(*[epochs, iou_scores, total_losses, L_data1_losses, L_data2_losses, L_cgan1_losses, D1_losses, G1_adv_losses, L_cgan2_losses, D2_losses, G2_adv_losses]))).add_prefix('Col')
+                df = pd.DataFrame(list(zip(*[epochs, iou_scores, total_losses, D_losses, G_losses, L_data1_losses,
+                                             L_data2_losses, L_cgan1_losses, D1_losses, G1_adv_losses, L_cgan2_losses,
+                                             D2_losses, G2_adv_losses]))).add_prefix('Col')
                 filename = path + 'G1D1G2D2_e' + str(epoch) + '_' + time_begin + '.csv'
                 print('saving to', filename)
                 df.to_csv(filename, index=False)
@@ -280,7 +282,7 @@ def single_gpu_train():
 
                 # saves lists of average (per epoch) losses
                 df = pd.DataFrame(list(zip(
-                    *[ave_epochs, ave_iou_scores, ave_total_losses, ave_L_data1_losses, ave_L_data2_losses,
+                    *[ave_epochs, ave_iou_scores, ave_total_losses, ave_D_losses, ave_G_losses, ave_L_data1_losses, ave_L_data2_losses,
                       ave_L_cgan1_losses, ave_D1_losses, ave_G1_adv_losses, ave_L_cgan2_losses, ave_D2_losses,
                       ave_G2_adv_losses]))).add_prefix('Col')
                 filename = path + 'G1D1G2D2_ave_e' + str(epoch) + '_' + time_begin + '.csv'
@@ -293,7 +295,7 @@ def single_gpu_train():
                 val_epochs.append(epoch)
                 val_iou_scores.append(run_iou_score / len(val_data_loader))
                 val_total_losses.append(run_loss / len(val_data_loader))
-                
+
                 val_D_losses.append(run_D_loss / len(val_data_loader))
                 val_G_losses.append(run_G_loss / len(val_data_loader))
 
@@ -308,9 +310,9 @@ def single_gpu_train():
 
                 # saves lists of average (per epoch) losses
                 df = pd.DataFrame(list(zip(
-                    *[val_epochs, val_iou_scores, val_total_losses, val_L_data1_losses, val_L_data2_losses,
-                      val_L_cgan1_losses, val_D1_losses, val_G1_adv_losses, val_L_cgan2_losses, val_D2_losses,
-                      val_G2_adv_losses]))).add_prefix('Col')
+                    *[val_epochs, val_iou_scores, val_total_losses, val_D_losses, val_G_losses, val_L_data1_losses,
+                      val_L_data2_losses, val_L_cgan1_losses, val_D1_losses, val_G1_adv_losses, val_L_cgan2_losses,
+                      val_D2_losses, val_G2_adv_losses]))).add_prefix('Col')
                 filename = path + 'G1D1G2D2_val_e' + str(epoch) + '_' + time_begin + '.csv'
                 print('saving to', filename)
                 df.to_csv(filename, index=False)
